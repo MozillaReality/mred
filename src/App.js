db0d0aa8c14e292593ec33b577588177aa57fce9
Josh Marinacci <joshua@marinacci.org>
2018-07-09 14:10:54 -0700

    cleanup app.js imports remove the document switcher icon since this app only has one document type

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
 import React, {Component} from 'react'
 import './App.css'
-import {PopupManager} from "appy-comps"
-import PropSheet from './PropSheet'
-import TreeTable from "./TreeTable"
-import {TREE_ITEM_PROVIDER} from './TreeItemProvider'
-import {PopupContainer, HBox, VBox, HToggleGroup} from "appy-comps"
-
-import SVGEditor from "./svg/SVGEditor"
-import HypercardEditor from "./h2d/HypercardEditor"
-import Hypercard3DEditor from "./h3d/Hypercard3DEditor"
-import FamilyTree from "./familytree/FamilyTree"
-import TextureEditor from "./texture/TextureEditor"
-import {toQueryString} from './utils'
-import {MenuPopup} from './GridEditorApp'
-import GLTFInspector from './gltfinspector/GLTFInspector'
+import {PopupContainer, PopupManager, VBox} from "appy-comps"
 import {Editor360Provider} from './360/Editor360Editor'
 
 export default class App extends Component {
     constructor(props) {
         super(props)
         this.providers = {}
-        // this.addProvider(new Hypercard3DEditor())
-        // this.addProvider(new HypercardEditor())
-        // this.addProvider(new SVGEditor())
-        // this.addProvider(new FamilyTree())
-        // this.addProvider(new TextureEditor())
-        // this.addProvider(new GLTFInspector())
         this.addProvider(new Editor360Provider())
         this.state = {
-            provider: null,//this.providers.familytree,
+            provider: null,
             providerName: '360',
         }
     }
@@ -77,12 +58,6 @@ export default class App extends Component {
         const content = this.state.provider.getApp()
         return (
             <VBox fill>
-                <button
-                    style={{position:'absolute',zIndex: 10,}}
-                    onClick={this.showProviderList}
-                    >
-                    <img src="icon.png" height="20" style={{padding:'0em'}}/>
-                </button>
                 <div style={{position: 'relative', flex: '1'}}>{content}</div>
                 <PopupContainer/>
             </VBox>
