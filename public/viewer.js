19e79045ec4534505848d260f1119aab7a2a25fd
Josh Marinacci <joshua@marinacci.org>
2018-08-14 15:07:28 -0700

    adjust rotation of background to match 2d view

--- public/viewer.js
+++ public/viewer.js
@@ -168,6 +168,7 @@ class SceneViewer {
             src: url,
             side: 'back'
         })
+        el.setAttribute('rotation',{x:0, y:90, z:0})
         el.setAttribute('scale',{x:-1,y:1, z:1})
         $('#children').appendChild(el)
         this.els_to_nodes[el.getAttribute('id')] = prim
