d4356a07030b754c62dc8367206c1068232a7440
Josh Marinacci <joshua@marinacci.org>
2019-02-06 11:26:08 -0800

    clean up multi-line code

--- src/metadoc/TextDef.js
+++ src/metadoc/TextDef.js
@@ -28,7 +28,33 @@ export default class TextDef {
     }
 
     draw(c,g,shape,selected) {
+        //calculate the general line metrics
+        const {lines, size_px, total_height, total_width} = this.calculateLines(shape,c)
+        //add offsets for alignment
+        lines.forEach((line,i) => {
+            const lines_height = lines.length*size_px
+            line.xoff = 0
+            if(shape.horizontalAlign === HORIZONTAL_ALIGNMENT.LEFT)   line.xoff = 0
+            if(shape.horizontalAlign === HORIZONTAL_ALIGNMENT.CENTER) line.xoff = (total_width - line.width)/2
+            if(shape.horizontalAlign === HORIZONTAL_ALIGNMENT.RIGHT)  line.xoff = total_width  - line.width
+            if(shape.verticalAlign === VERTICAL_ALIGNMENT.TOP)        line.yoff = 0
+            if(shape.verticalAlign === VERTICAL_ALIGNMENT.CENTER)     line.yoff = (total_height- lines_height)/2
+            if(shape.verticalAlign === VERTICAL_ALIGNMENT.BASELINE)   line.yoff = total_height - lines_height
+            if(shape.verticalAlign === VERTICAL_ALIGNMENT.BOTTOM)     line.yoff = total_height - lines_height
+            line.yoff += (i+1)*line.height
+        })
+
+        //draw text
         c.fillStyle = shape.fillColor
+        lines.forEach((line,i) => c.fillText(line.text,shape.x+line.xoff,shape.y+ line.yoff))
+        //draw outline
+        if (selected) {
+            c.strokeStyle = 'red'
+            c.strokeRect(shape.x + 0.5, shape.y + 0.5, total_width, total_height)
+        }
+    }
+
+    calculateLines(shape,c) {
         const size_px = shape.fontSize
         c.font = `normal ${size_px}px sans-serif`
         const lines = shape.text.split("\n").map(text => {
@@ -48,60 +74,28 @@ export default class TextDef {
             total_width = shape.width
             total_height = shape.height
         }
-        lines.forEach((line,i) => {
-            const lines_height = lines.length*size_px
-            line.xoff = 0
-            if(shape.horizontalAlign === HORIZONTAL_ALIGNMENT.LEFT) {
-                line.xoff = 0
-            }
-            if(shape.horizontalAlign === HORIZONTAL_ALIGNMENT.CENTER) {
-                line.xoff = (total_width - line.width)/2
-            }
-            if(shape.horizontalAlign === HORIZONTAL_ALIGNMENT.RIGHT) {
-                line.xoff = total_width - line.width
-            }
-            if(shape.verticalAlign === VERTICAL_ALIGNMENT.TOP) {
-                line.yoff = 0
-            }
-            if(shape.verticalAlign === VERTICAL_ALIGNMENT.CENTER) {
-                line.yoff = (total_height-lines_height)/2
-            }
-            if(shape.verticalAlign === VERTICAL_ALIGNMENT.BASELINE) {
-                line.yoff = total_height - lines_height
-            }
-            if(shape.verticalAlign === VERTICAL_ALIGNMENT.BOTTOM) {
-                line.yoff = total_height - lines_height
-            }
-            line.yoff += (i+1)*line.height
-        })
-
-        lines.forEach((line,i) => c.fillText(line.text,shape.x+line.xoff,shape.y+ line.yoff))
-        if (selected) {
-            c.strokeStyle = 'red'
-            c.strokeRect(shape.x + 0.5, shape.y + 0.5, total_width, total_height)
+        return {
+            total_width:total_width,
+            total_height: total_height,
+            lines:lines,
+            size_px:size_px
         }
+    }
+
+    insideRect(pt,x,y,w,h) {
+        if (pt.x < x) return false
+        if (pt.x > x + w) return false
+        if (pt.y < y) return false
+        if (pt.y > y + h) return false
+        return true
     }
 
     isInside(pt,shape, canvas) {
         const c = canvas.getContext('2d')
-        c.font = `normal ${shape.fontSize}px sans-serif`
-        const metrics = c.measureText(shape.text)
-        const w = metrics.width
-        const h = shape.fontSize
-        if(shape.autoFit) {
-            if (pt.x < shape.x) return false
-            if (pt.x > shape.x + w) return false
-            if (pt.y < shape.y) return false
-            if (pt.y > shape.y+h) return false
-            return true
-        } else {
-            if (pt.x < shape.x) return false
-            if (pt.x > shape.x + shape.width) return false
-            if (pt.y < shape.y) return false
-            if (pt.y > shape.y + shape.height) return false
-            return true
-        }
+        const {total_width, total_height} = this.calculateLines(shape,c)
+        return this.insideRect(pt,shape.x,shape.y,total_width,total_height)
     }
+
     toSVGString(obj) {
         return `<text x="${obj.x}" y="${obj.y}"
                      fill="${obj.fillColor}" fontSize="${obj.fontSize}"
