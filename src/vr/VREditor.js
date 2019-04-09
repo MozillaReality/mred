307b4188d7e759881bb7c7a73fb754d92b2ee489
Josh Marinacci <joshua@marinacci.org>
2019-04-09 14:21:07 -0700

    make bg of grid transparent add option to make panel transparent

--- src/vr/VREditor.js
+++ src/vr/VREditor.js
@@ -668,7 +668,7 @@ class VREditorApp extends Component {
             </Toolbar>
 
 
-            <Panel center middle scroll>
+            <Panel center middle scroll transparent>
                 {this.renderCenterPane(this.state.mode)}
             </Panel>
 
