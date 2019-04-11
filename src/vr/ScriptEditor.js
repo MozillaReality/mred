356e64c8e875fccd4713219e36e1c4dfd78c0a12
Josh Marinacci <joshua@marinacci.org>
2019-04-11 14:02:24 -0700

    update the scriptBody property in the script editor only on focus blur, instead of every keystroke

--- src/vr/ScriptEditor.js
+++ src/vr/ScriptEditor.js
@@ -6,34 +6,40 @@ import selMan, {SELECTION_MANAGER} from '../SelectionManager'
 export default class ScriptEditor extends Component {
     constructor(props) {
         super(props)
-        this.h2 = () => this.setState({selection:selMan.getSelection()})
+        this.refresh = () => {
+            const id = selMan.getSelection()
+            const action = fetchGraphObject(this.props.provider.getDataGraph(),id)
+            this.setState({id:selMan.getSelection(), text:action.scriptBody})
+        }
         this.state = {
-            selection:null,
+            id:null,
+            text:'empty',
         }
     }
     componentDidMount() {
-        this.setState({selection:selMan.getSelection()})
-        this.props.provider.on(TREE_ITEM_PROVIDER.PROPERTY_CHANGED,this.h2)
+        this.refresh()
+        this.props.provider.on(TREE_ITEM_PROVIDER.PROPERTY_CHANGED,this.refresh)
     }
 
     componentWillUnmount() {
-        this.props.provider.off(TREE_ITEM_PROVIDER.PROPERTY_CHANGED,this.h2)
+        this.props.provider.off(TREE_ITEM_PROVIDER.PROPERTY_CHANGED,this.refresh)
     }
-    changeText = (e) => {
-        const acc = this.props.provider.accessObject(this.state.selection)
-        this.props.provider.quick_setPropertyValue(this.state.selection,'scriptBody',e.target.value)
+    changeText = (e) => this.setState({text:e.target.value})
+    save = (e) => {
+        const acc = this.props.provider.accessObject(this.state.id)
+        this.props.provider.quick_setPropertyValue(this.state.id,'scriptBody',this.state.text)
     }
     render() {
         const prov = this.props.provider
-        if(this.state.selection === null) return <div>loading</div>
-        const action = fetchGraphObject(prov.getDataGraph(),this.state.selection)
+        if(this.state.id === null) return <div>loading</div>
         return <div>
-            <textarea value={action.scriptBody}
+            <textarea value={this.state.text}
                       style={{
                           width:'99%',
                           height:'600px',
                       }}
                       onChange={this.changeText}
+                      onBlur={this.save}
             />
         </div>
     }
