c68ee4394716d5dc27a55e3e1a93aaedf96123a1
Josh Marinacci <joshua@marinacci.org>
2019-05-16 13:42:00 -0700

    change console.log to logger.log

--- src/vr/ImmersivePlayer.js
+++ src/vr/ImmersivePlayer.js
@@ -125,12 +125,12 @@ export class ImmersivePlayer extends Component {
         if(intersect && intersect.length >= 1) {
             const inter = intersect.find(inter => inter.distance > 0)
             if(inter) {
-                console.log("found the interesction", inter)
-                console.log("intersections", intersect)
+                this.logger.log("found the interesction", inter)
+                this.logger.log("intersections", intersect)
                 const obj = inter.object
-                console.log("obj", obj)
+                this.logger.log("obj", obj)
                 const gobj = this.obj_map[obj.userData.graphid]
-                console.log("jobj", gobj)
+                this.logger.log("jobj", gobj)
                 this.scriptManager.performClickAction(gobj)
             }
         }
@@ -417,7 +417,7 @@ class Adapter extends SceneGraphProvider {
 
     stopGeoRecognizer() {
         //TODO: @ahook
-        console.log("WE NEED TO STOP ALL geo recognizers here")
+        this.logger.log("WE NEED TO STOP ALL geo recognizers here")
     }
 
 }
