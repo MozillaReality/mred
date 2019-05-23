dfef1bf71b8f45df32d7574cc1bedcb62673639d
Josh Marinacci <joshua@marinacci.org>
2019-05-23 12:48:48 -0700

    update build

---
+++ build/precache-manifest.f287dc61c33a687775b66b351e2cdea7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "2909d46d5017da592b69",
+    "url": "./static/css/main.48a4eaa3.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "7278e66f36208b16c4ac",
+    "url": "./static/js/1.7278e66f.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "2909d46d5017da592b69",
+    "url": "./static/js/main.2909d46d.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "7278e66f36208b16c4ac",
+    "url": "./static/css/1.b0f70251.chunk.css"
+  },
+  {
+    "revision": "6f0e6c7e822f4a2c6db24d68f8c2fb1e",
+    "url": "./index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f249f19c78140e7b36dc4ef1133b14d6.js"
+  "./precache-manifest.f287dc61c33a687775b66b351e2cdea7.js"
 );
 
 workbox.clientsClaim();
