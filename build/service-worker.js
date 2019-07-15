d227e5b9441ec893524e4700632b187de3da7001
joshmarinacci <joshua@marinacci.org>
2019-07-15 14:29:30 -0700

    update the build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.bc16aedc227ac824e99d73152c659575.js"
+  "./precache-manifest.281bca1b49ef1ce279941d49a38ca4d5.js"
 );
 
 workbox.clientsClaim();
